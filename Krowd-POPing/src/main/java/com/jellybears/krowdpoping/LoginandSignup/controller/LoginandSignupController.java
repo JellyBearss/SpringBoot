9bf6dd26a80fa23b36c01148b95a268a562b56ac
CHUWENCHING <chuuu3619@gmail.com>
2023-12-05 15:04:04 +0900

    login&signup controller 생성

---
+++ Krowd-POPing/src/main/java/com/jellybears/krowdpoping/LoginandSignup/controller/LoginandSignupController.java
@@ -0,0 +1,28 @@
+package com.jellybears.krowdpoping.LoginandSignup.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+@RequestMapping("/loginandsignup/*")
+public class LoginandSignupController {
+
+        /**
+         * 로그인 하기
+         */
+        @GetMapping("login")
+        public String login() {
+
+            return "/loginandsignup/LoginandSignup.html";
+        }
+
+        /**
+         * 회원가입 이용약관 조회 페이지
+         */
+        @GetMapping("termofservice")
+        public String TermOfService() {
+
+            return "/loginandsignup/Signup_1.html";
+        }
+
+}
