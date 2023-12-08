eaca5e6a831535bad7325eba576fda237e41d818
yeji <yyj87922@naver.com>
2023-12-08 10:35:16 +0900

    yeji page link update4

--- Krowd-POPing/src/main/java/com/jellybears/krowdpoping/creatorinfomation/model/service/CreatorService.java
+++ Krowd-POPing/src/main/java/com/jellybears/krowdpoping/creatorinfomation/model/service/CreatorService.java
@@ -1,25 +1,25 @@
-package com.jellybears.krowdpoping.creatorinfomation.model.service;
-
-import com.jellybears.krowdpoping.creatorinfomation.model.dao.CreatorMapper;
-import com.jellybears.krowdpoping.creatorinfomation.model.dto.CreatorDTO;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class CreatorService {
-
-    private final CreatorMapper creatorMapper;
-
-    public String getCreator() {
-        String creatorDTO = creatorMapper.selectCreator();
-       // creatorDTO.setAddress("asdf");
-
-        return creatorDTO;
-    }
-
-    public void setCreator(CreatorDTO creatorDTO) {
-        creatorMapper.insertCreator(creatorDTO);
-    }
-
-}
+//package com.jellybears.krowdpoping.creatorinfomation.model.service;
+//
+//import com.jellybears.krowdpoping.creatorinfomation.model.dao.CreatorMapper;
+//import com.jellybears.krowdpoping.creatorinfomation.model.dto.CreatorDTO;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class CreatorService {
+//
+//    private final CreatorMapper creatorMapper;
+//
+//    public String getCreator() {
+//        String creatorDTO = creatorMapper.selectCreator();
+//       // creatorDTO.setAddress("asdf");
+//
+//        return creatorDTO;
+//    }
+//
+//    public void setCreator(CreatorDTO creatorDTO) {
+//        creatorMapper.insertCreator(creatorDTO);
+//    }
+//
+//}
