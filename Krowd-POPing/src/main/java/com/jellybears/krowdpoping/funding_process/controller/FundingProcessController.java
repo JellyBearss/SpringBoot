1f6a4192501fe4dbd6de36934d0d65ef5b01a0db
inji-yeon <inji2349@gmail.com>
2023-12-11 14:57:28 +0900

    checkbox disabled modified

--- Krowd-POPing/src/main/java/com/jellybears/krowdpoping/funding_process/controller/FundingProcessController.java
+++ Krowd-POPing/src/main/java/com/jellybears/krowdpoping/funding_process/controller/FundingProcessController.java
@@ -1,22 +1,15 @@
 package com.jellybears.krowdpoping.funding_process.controller;
 
-import com.jellybears.krowdpoping.funding_process.model.dto.AddressDTO;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.client.RestTemplate;
-
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
 
 @Controller
 @RequestMapping("funding_process/*")
 public class FundingProcessController {
 
     @GetMapping("address")
-    public String defaultAddress() { return "/funding_process/address"; }
+    public String defaultAddress() { return "/funding_process/default_address"; }
 
     @GetMapping("product")
     public String detailedProduct(){
