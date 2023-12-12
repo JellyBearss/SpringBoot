33adf6818c8ef782e00cc9398bcecdac8da7ef68
dev-dahee <dev_dahee@naver.com>
2023-12-12 10:38:53 +0900

    2023/12/08 noticeList 조회

---
+++ Krowd-POPing/src/main/java/com/jellybears/krowdpoping/common/exception/admin/notice/NoticeModifyException.java
@@ -0,0 +1,8 @@
+package com.jellybears.krowdpoping.common.exception.admin.notice;
+
+public class NoticeModifyException extends Exception {
+
+    public NoticeModifyException() {}
+
+    public NoticeModifyException(String msg) { super(msg); }
+}
---
+++ Krowd-POPing/src/main/java/com/jellybears/krowdpoping/common/exception/admin/notice/NoticeRegistException.java
@@ -0,0 +1,8 @@
+package com.jellybears.krowdpoping.common.exception.admin.notice;
+
+public class NoticeRegistException extends Exception {
+
+    public NoticeRegistException() {}
+
+    public NoticeRegistException(String msg) { super(msg);}
+}
