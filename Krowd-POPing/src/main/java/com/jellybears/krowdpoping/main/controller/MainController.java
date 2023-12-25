1e85a19b96d23fcaab4bc599669e6226f1261c39
yeji <yyj87922@naver.com>
2023-12-26 00:31:17 +0900

    2023-12-26 yeji commit3

--- Krowd-POPing/src/main/java/com/jellybears/krowdpoping/main/controller/MainController.java
+++ Krowd-POPing/src/main/java/com/jellybears/krowdpoping/main/controller/MainController.java
@@ -1,21 +1,32 @@
 package com.jellybears.krowdpoping.main.controller;
 
-import org.springframework.security.crypto.password.PasswordEncoder;
+import com.jellybears.krowdpoping.category.model.dto.CaProjectDTO;
+import com.jellybears.krowdpoping.main.model.service.MainService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("krowdpoping")
 public class MainController {
 
+    private final MainService mainService;
+
+    public MainController(MainService mainService) {
+        this.mainService = mainService;
+    }
+
     @GetMapping("mainpage")
 
-    public String goMain() {
+    public String goMain(Model model) {
 
-
-
-
+//        List<CaProjectDTO> mainAllProjectList = mainService.getMainAllProjectList();
+//        model.addAttribute("mainAllProjectList", mainAllProjectList);
+//
+//        System.out.println("mainAllProjectList = " + mainAllProjectList);
 
         return "main/main";
     }
---
+++ Krowd-POPing/src/main/java/com/jellybears/krowdpoping/main/model/service/MainService.java
@@ -0,0 +1,14 @@
+package com.jellybears.krowdpoping.main.model.service;
+
+import com.jellybears.krowdpoping.category.model.dto.CaProjectDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MainService {
+//    public List<CaProjectDTO> getMainAllProjectList() {
+//
+//
+//    }
+}
